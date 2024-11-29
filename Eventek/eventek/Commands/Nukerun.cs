66a18512664b54766a94b4f2b546f5d5a09a2a25
2Programer <hestegimposztor2@gmail.com>
2024-11-29 15:49:10 +0100

    yes

--- Eventek/eventek/Commands/Nukerun.cs
+++ Eventek/eventek/Commands/Nukerun.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommandSystem;
+using PluginAPI.Core.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace eventek.Commands
 {
-    internal class Nukerun
+    internal class Nukerun : ICommand
     {
+        public string Command { get; set; } = "NR";
+        public string Description { get; set; } = "yes";
+        public string[] Aliases { get; set; } =  new string[] { "yes" };
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = null;
+            return true;
+        }
     }
 }
